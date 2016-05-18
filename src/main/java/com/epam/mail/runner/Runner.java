58d8eea8a9fd5059853978591979a3b2fc70f9f2
Dauren_Altynbekov <Dauren_Altynbekov@epam.com>
2016-05-18 13:49:00 +0600

    fixed pom

--- src/main/java/com/epam/mail/runner/Runner.java
+++ src/main/java/com/epam/mail/runner/Runner.java
@@ -3,9 +3,6 @@ package com.epam.mail.runner;
 import com.epam.mail.sender.Arguments;
 import com.epam.mail.sender.MailSender;
 import com.epam.util.ArgumentParser;
-import org.apache.log4j.Logger;
-
-import java.util.Map;
 
 /**
  * Created by Dauren_Altynbekov on 5/17/2016.
